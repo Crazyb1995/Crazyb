c80b8edc64eab42475cd2cc56ea68b94329a8985
Crazyb1995 <2544736785@qq.com>
2019-09-17 16:39:44 +0800

    login controller

---
+++ src/main/java/com/scu/crazyb/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.scu.crazyb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+    
+    @RequestMapping(value = "/login")
+    public String loginJsp(){
+        return "login";
+    }
+}
